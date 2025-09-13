58b1582e8d4d327439b8c6d2669f9a9b756fbed8
Prince <princekumaraman7654@gmail.com>
2025-09-13 23:33:20 +0530

    initial project setup done

---
+++ src/test/java/com/Curio/CurioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Curio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
